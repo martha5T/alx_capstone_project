e60a53cda4a7af4ea9a6631aa34a1eb93b139471
Martha Siiyabia Tong <marthasiiyatong@gmail.com>
2023-10-20 01:08:46 -0700

    script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
 const header = document.querySelector("header")
 
 windows.addEventListener ("scroll", function() {header.classList.toggle ("sticky", windows.scrolly > 120);});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
+}
